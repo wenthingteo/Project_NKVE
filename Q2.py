0fb3eda8339cb69811e3e7158dd9f58928768769
kerryyys <ttkkyy604@gamil.com>
2024-05-08 23:54:30 +0800

    modified Q2 according to Q3 120 solution stated

--- Q2.py
+++ Q2.py
@@ -1,35 +1,14 @@
 def generate_combinations():
-    total_combinations = 0
-    combinations = []
+    count = 0
+    for i in range(10):
+        for j in range(i + 1, 10):
+            for k in range(j + 1, 10):
+                count += 1
+                print(f"{i}{j}{k}", end=" ")
+                if count % 30 == 0:
+                    print() 
+    print("\nTotal number of combinations:", count)
 
-    combination = [0, 0, 0]
-
-    while combination != [9, 9, 9]:
-        combinations.append(tuple(combination))
-        total_combinations += 1
-
-        for i in range(2, -1, -1):
-            if combination[i] < 9:
-                combination[i] += 1
-                break
-            else:
-                combination[i] = 0
-
-        if total_combinations % 35 == 0:
-            process_combinations(combinations)
-            combinations = []
-
-    combinations.append((9, 9, 9))
-    total_combinations += 1
-    process_combinations(combinations)
-
-    return total_combinations
-
-def process_combinations(combinations):
-    print(" ".join("".join(str(digit) for digit in combination) for combination in combinations))
-
-total_combinations = generate_combinations()
-print("\nTotal number of combinations:", total_combinations)
-
+generate_combinations()
 
 
